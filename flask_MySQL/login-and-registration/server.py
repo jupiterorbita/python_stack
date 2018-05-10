97e707c1cab2c4e396d37e29c741020914bac761
jupterorbita <john.williams@outlook.com>
2018-05-10 13:29:42 -0700

    completed login-registration

--- flask_MySQL/login-and-registration/server.py
+++ flask_MySQL/login-and-registration/server.py
@@ -13,16 +13,9 @@ print('\n','= = = server start = = = server.py ')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/register', methods=['POST'])
-def register():
-    print('\n= = = = GOT POST INFO = = = = ')
-    print(request.form)
-    print('= = = = END OF POST INFO = = = =')
     # ======= session checks ==========
     # first name check session
-    if 'fist_name' not in session:
+    if 'first_name' not in session:
         session['first_name'] = ''
     # last name check session
     if 'last_name' not in session:
@@ -30,11 +23,22 @@ def register():
     # email check session
     if 'email' not in session:
         session['email'] = ''
+    # pass vars to browser to remember values
+    return render_template('index.html', temp_fname=session['first_name'], temp_lname=session['last_name'], temp_email=session['email'])
+
+@app.route('/register', methods=['POST'])
+def register():
+    print('\n= = = = GOT POST INFO = = = = ')
+    print(request.form)
+    print('= = = = END OF POST INFO = = = =')
+    
 
     # ========== validation checks ===========
     # flag set
     validationError = False
-    # --------------- first name validation
+    passValidationError = False
+
+    # --------------- FIRST NAME validation
     if request.form['first_name'] == '':
         print('@@@@@@@ FIRST is empty ----')
         flash('First name cannot be empty!')
@@ -47,7 +51,8 @@ def register():
         print('@@@@@@@ FIRST is not a string -----') 
         flash('First name must contain ONLY LETTERS')
         validationError = True
-    # --------------- last name validation
+
+    # --------------- LAST NAME validation
     if request.form['last_name'] == '':
         print('@@@@@@@ LAST name is empty ----')
         flash('Last name cannot be empty!')
@@ -60,18 +65,20 @@ def register():
         print('@@@@@@@ LAST is not a string -----') 
         flash('Last name must contain ONLY LETTERS')
         validationError = True
-    # ---------------- email validation
+
+    # ---------------- EMAIL validation
     if len(request.form['email']) < 1:
         flash('email cannot be blank!')
         validationError = True
     elif not EMAIL_REGEX.match(request.form['email']):
         flash('invalid email address!')
         validationError = True
+    # ========= check what email we are getting
     query_email = "SELECT email FROM users WHERE email='{}';".format(request.form['email'])
     print(query_email)
     email_check = mysql.query_db(query_email)
     print('("@@@@@@@@@@@@@@@@@@@@  email_check check is: ', email_check)
-
+    #============ EMAILcheck if in DB ===============
     if len(email_check) > 0:
         print('@@@@@@@@@@@@@@@@@@@@  MATCH -- email_check check is: ', email_check[0])
         flash('email already in use - cannot use this email !!!')
@@ -79,26 +86,81 @@ def register():
     else:
         print('@@@@@@@@@@@@@@@@@@@@ NO match -- ')
     print("@@@@@@@@@@@@@@@@@@@@ DONE email check")
+    # ----------------- PASSWORD validation
 
-    # ################### pw validation
-    # pw_hash = bcrypt.generate_password_hash(request.form['password'])  
-    # print('############# pw_hash =', pw_hash) 
+    # ################## PASSWORD validation #####################################
+    if len(request.form['password1']) <1:
+        print('@@@@@@@@@@@@@@@ PW1 is empty')
+        flash('PW1 is empty')
+        validationError = True
+    if request.form['password1'] != request.form['password2']:
+        flash('@@@@@@@@ pw do not match!')
+        print('@@@@@@@@ PW DO NOT MATCH')
+        validationError = True
+        #if pw match...
+    if request.form['password1'] == request.form['password2']:
+        print('@@@@@@@@@ PASSWORDS MATCH!!! @@@@@ now hash pw1 and store it!')
+        # ...CREATE HASH
+        # pw_hash = bcrypt.generate_password_hash(request.form['password'])  
+        # print('############# pw_hash =', pw_hash) 
+        # query = "INSERT INTO users (username, password) VALUES (%(username)s, %(password_hash)s);"
+        # # put the pw_hash in our data dictionary, NOT the password the user providedcopy
+        # data = { "username" : request.form['username'],"password_hash" : pw_hash }
+        # mysql.query_db(query, data)
+    # never render on a post, always redirect!
+
+    # ====== retain sessions for FIRST, LAST, EMAIL in case of error
+    session['first_name'] = request.form['first_name']
+    session['last_name'] = request.form['last_name']
+    session['email'] = request.form['email']
+
 
     # =-=-=-=-=-=- FINAL VALIDATION =-=-=---=-=-=-=
     if validationError == False:
-        print('@@@@@@@@@@@@@ passed all validations @@@@')
-        query = "INSERT INTO users (first_name, last_name, email, created_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, NOW()); "
-        data = {'first_name': request.form['first_name'], 'last_name': request.form['last_name'], 'email': request.form['email'], 'created_at': '' }
-        mysql.query_db(query, data)
-
-
+        if request.form['password1'] == request.form['password2']:
+            print('@@@@@@@@@ PASSWORDS MATCH!!! @@@@@ now hash pw1 and store it!')
+             # if pw match CREATE HASH
+            pw_hash = bcrypt.generate_password_hash(request.form['password1'])  
+            print('############# pw_hash =', pw_hash) 
+            query = "INSERT INTO users (first_name, last_name, email, created_at, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, NOW(), %(password_hash)s);"
+            # put the pw_hash in our data dictionary, NOT the password the user providedcopy
+            # insert users & PW into DB
+            data = { 'first_name': request.form['first_name'], 'last_name': request.form['last_name'], 'email': request.form['email'], 'created_at': '', "password_hash" : pw_hash }
+            mysql.query_db(query, data)
+            print('@@@@@@@@@@@@@ passed all validations @@@@')
+        return redirect('/success')
+    
     return redirect('/')
 
 
-@app.route('/login', methods=['post'])
+@app.route('/login', methods=['POST'])
 def login():
-    return render_template('success.html')
+    query = "SELECT * FROM users WHERE email = %(email)s;"
+    data = { "email" : request.form["email"] }
+    result = mysql.query_db(query, data)
+    if result:
+        if bcrypt.check_password_hash(result[0]['password'], request.form['password3']):
+            session['userid'] = result[0]['id']
+            
+            # this retrieves the first name that matches the email given from post:
+            query_fname = "SELECT first_name FROM users WHERE email='{}';".format(request.form['email'])
+            print(query_fname)
+            fname_check = mysql.query_db(query_fname)
+            # fname_check[0]['first_name'] -> gets the first name from 0 index key 'first_name'
+            print('("@@@@@@@@@@@@@@@@@@@@  Fname_check check is: ', fname_check[0]['first_name'])
 
+            session['first_name'] = fname_check[0]['first_name']
+
+            
+            # session['name'] = name
+            return redirect('/success')
+        flash("You could not be logged in")
+        return redirect("/")
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 @app.route('/logout')
 def logout():
