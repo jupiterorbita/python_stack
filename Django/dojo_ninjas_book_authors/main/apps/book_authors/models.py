a8075c6b06ae2a8a0a2fd4323cbf29d3a764124e
jupterorbita <john.williams@outlook.com>
2018-05-16 22:15:35 -0700

    added ninja-book-authors

---
+++ Django/dojo_ninjas_book_authors/main/apps/book_authors/models.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+from django.db import models
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def repr(self):
+        return "<Book object: {} {} {} {}>".format(self.name, self.desc, self.created_at, self.updated_at)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    books = models.ManyToManyField(Book, related_name="authors")
+    def repr(self):
+        return "<Author object: {} {} {} {} {}>".format(self.first_name, self.last_name, self.email, self.created_at, self.updated_at)
