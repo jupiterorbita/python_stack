554b2dbd1853fbae6864c3bd64b25a91d7cbebb0
jupterorbita <john.williams@outlook.com>
2018-05-22 13:01:56 -0700

    added BeltReviewer

--- Django/belt_reviewer/main/apps/beltreviewer/views.py
+++ Django/belt_reviewer/main/apps/beltreviewer/views.py
@@ -42,85 +42,100 @@ def registration(request):
 
 
 def books(request):
-    print('\n = = = inside BOOKS ("/books") = = = /books.html')
-    # show all books avail
-    bookDB = Book.objects.all().order_by("-created_at")
-    context = {
-        'books': bookDB
-    }
-    return render(request, 'beltreviewer/books.html', context)
+    if 'id' in request.session:
+        print('\n = = = inside BOOKS ("/books") = = = /books.html')
+        # show all books avail
+        bookDB = Book.objects.all().order_by("-created_at")
+        bookreveiw = Book.objects.get(id=22).reviews.all()
+        context = {
+            'books': bookDB
+        }
+        return render(request, 'beltreviewer/books.html', context)
+    else:
+        return redirect('/')
 
 
 
 def add_book(request):
-    print('\n = = = inside ADD_BOOK ("/add_book") /add_book.html = = =')
-    return render(request, 'beltreviewer/add_book.html')
+    if 'id' in request.session:
+        print('\n = = = inside ADD_BOOK ("/add_book") /add_book.html = = =')
+        return render(request, 'beltreviewer/add_book.html')
+    else:
+        return redirect('/')
 
 
 def add_book_method(request):
-    print('\n = = = inside ADD_BOOK_METHOD ("/add_book") method only  = = =')
-    #logic for adding a book from a user
-    if request.method == 'POST':
-        print(request.POST)
-        errors = Book.objects.book_validator(request.POST)
-        print(errors)
-        if len(errors):
-            for key, value in errors.items():
-                messages.error(request, value)
-            return redirect('/add_book')
-        else:
-            errors = Review.objects.review_validator(request.POST)
-            new_book = Book.objects.create(title=request.POST['title'], author=request.POST['author'], creator=User.objects.get(id=request.session['id']))
-            print('WHAT IS NEWBOOK.ID >>!>>!>!>>>!>>!> ', new_book.id)
+    if 'id' in request.session:
+        print('\n = = = inside ADD_BOOK_METHOD ("/add_book") method only  = = =')
+        #logic for adding a book from a user
+        if request.method == 'POST':
+            print(request.POST)
+            errors = Book.objects.book_validator(request.POST)
+            print(errors)
             if len(errors):
                 for key, value in errors.items():
                     messages.error(request, value)
                 return redirect('/add_book')
             else:
-                Review.objects.create(review=request.POST['review'], rating=request.POST['rating'], book=new_book, user=User.objects.get(id=request.session['id']))
-                
-                print('***** UPLOAD SUCCESS ******')
-                return redirect('/book_review/'+str(new_book.id))
-
+                errors = Review.objects.review_validator(request.POST)
+                new_book = Book.objects.create(title=request.POST['title'], author=request.POST['author'], creator=User.objects.get(id=request.session['id']))
+                print('WHAT IS NEWBOOK.ID >>!>>!>!>>>!>>!> ', new_book.id)
+                if len(errors):
+                    for key, value in errors.items():
+                        messages.error(request, value)
+                    return redirect('/add_book')
+                else:
+                    Review.objects.create(review=request.POST['review'], rating=request.POST['rating'], book=new_book, user=User.objects.get(id=request.session['id']))
+                    
+                    print('***** UPLOAD SUCCESS ******')
+                    return redirect('/book_review/'+str(new_book.id))
+    else:
+        return redirect('/')
 
 def book_review(request, id):
-    print('\n = = = inside BOOK_REVIEW ("/book_review") /book_review.html = = =')
-    # you see the book you want to review
-    bookDB = Book.objects.get(id=id)
-    reviewDB = Book.objects.get(id=id).reviews.all()
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ USERS")
-    users = User.objects.get(id=4).reviews
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ USERS")
-    print(users)
-    context = {
-        'books': bookDB,
-        'reviews': reviewDB,
-        'users': users
-    }
-    return render(request, 'beltreviewer/book_review.html', context)
-
+    if 'id' in request.session:
+        print('\n = = = inside BOOK_REVIEW ("/book_review") /book_review.html = = =')
+        # you see the book you want to review
+        bookDB = Book.objects.get(id=id)
+        reviewDB = Book.objects.get(id=id).reviews.all()
+        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ USERS")
+        users = User.objects.get(id=4).reviews
+        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ USERS")
+        print(users)
+        context = {
+            'books': bookDB,
+            'reviews': reviewDB,
+            'users': users
+        }
+        return render(request, 'beltreviewer/book_review.html', context)
+    else:
+        return redirect('/')
 
 def add_book_review(request):
-    print('\n = = = inside add_book_review method = = =')
-    if request.method == 'POST':
-        book = Book.objects.get(id=request.POST['book_id']).id
-        print('\n=-=-=-= book =', book)
-        Review.objects.create(review=request.POST['review'], rating=request.POST['rating'], book=Book.objects.get(id=book), user = User.objects.get(id=request.session['id']))
-        return redirect('/book_review/'+str(book))
-
+    if 'id' in request.session:
+        print('\n = = = inside add_book_review method = = =')
+        if request.method == 'POST':
+            book = Book.objects.get(id=request.POST['book_id']).id
+            print('\n=-=-=-= book =', book)
+            Review.objects.create(review=request.POST['review'], rating=request.POST['rating'], book=Book.objects.get(id=book), user = User.objects.get(id=request.session['id']))
+            return redirect('/book_review/'+str(book))
+    else:
+        return redirect('/')
 
 def user(request, id):
-    print('\n = = = inside USER ("/user") = = =')
-    user = User.objects.get(id=id)
-    reviews = User.objects.get(id=id).reviews.all()
-    print('what is this?@@@@@@@@@@@@@ ', reviews)
-    context = {
-        'users': user,
-        'reviews': reviews
-    }
-    print('$$$$$$$$ context ===>', context)
-    return render(request, 'beltreviewer/user.html', context)
-
+    if 'id' in request.session:
+        print('\n = = = inside USER ("/user") = = =')
+        user = User.objects.get(id=id)
+        reviews = User.objects.get(id=id).reviews.all()
+        print('what is this?@@@@@@@@@@@@@ ', reviews)
+        context = {
+            'users': user,
+            'reviews': reviews
+        }
+        print('$$$$$$$$ context ===>', context)
+        return render(request, 'beltreviewer/user.html', context)
+    else:
+        return redirect('/')
 
 def logout(request):
     print('\n !!! LOGOUT method !!!!("/logout")')
---
+++ Django/the_wall/main/apps/wall/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, HttpResponse, redirect
+from .models import *
+from django.contrib import messages
+import bcrypt
+
+
+def index(request):
+    print('\n= = = index = = =')
+    return render(request, 'wall/index.html')
+
+
+def registration(request):
+    print('\ntouch regitration========')
+    if request.method == 'POST':
+        errors = User.objects.registration_validator(request.POST)
+        if len(errors):
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        else:
+            hashed_pw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+            User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'], bday=request.POST['bday'], password=hashed_pw)
+            request.session['id'] = User.objects.get(email=request.POST['email']).id
+            messages.success(request, "WELCOME FROM REGISTRATION")
+            return redirect('/success')
+
+
+def login(request):
+    print('\ntouch LOGIN============')
+    if request.method == 'POST':
+        errors = User.objects.login_validator(request.POST)
+        if len(errors):
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        else:
+            request.session['id'] = User.objects.get(email=request.POST['email']).id
+            messages.success(request, "WELCOME FROM LOGIN")
+            return redirect('/wall')
+
+
+def wall(request):
+    print('\ntouch SUCCESS=========')
+    if 'id' in request.session:
+        user = User.objects.get(id=request.session['id'])
+        context = {
+            'user': user,
+        }
+        return render(request, 'wall/wall.html', context)
+    else:
+        return redirect('/')
+
+
+
+def logout(request):
+    print('\n= = = LOGOUT = = =')
+    request.session.clear()
+    return redirect('/')
